e730ce70351b8e827610e456ff7efc6514dbd0be
Tala Taktak <talataktak39@gmail.com>
2025-10-04 11:47:43 +0300

    Added defintion

--- assignment2.py
+++ assignment2.py
@@ -5,8 +5,9 @@ while attempts < 3 :
    password = input ("Enter your password: ")
    if username == "admin" and password == "1234":
      print ("Login Successful! ")
-     
+
      N =  int ( input ("Enter a number N : "))
+     print ("Prime numbers between 2 and " , N , " are: " , end = " ")
      for d in range (2 , N +1 ) :
         is_prime = True;
         for i in range (2,d) :
